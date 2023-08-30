2c18197916073644b05989c0dc6c9db17b1d7351
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-08-30 15:27:27 -0500

    gui debugger implementation

---
+++ Readme.md
@@ -0,0 +1,2 @@
+In order to implement Dat.gui, a debugging tool, install the package and create an instance as gui.
+npm install lil-gui <- this is the latest option
--- script.js
+++ script.js
@@ -1,15 +1,57 @@
 import * as THREE from "three";
+import * as dat from "lil-gui"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import gsap from "gsap"
+
+
+// DAT GUI
+const gui = new dat.GUI()
+
+
+
 const canvas = document.querySelector(".webgl")
 
 const scene = new THREE.Scene()
 
 const geometry = new THREE.BoxGeometry(1,1,1,2,2,2)
-const material = new THREE.MeshBasicMaterial({ color: "red", wireframe: true })
+const material = new THREE.MeshBasicMaterial({ color: "red" })
 
 const mesh = new THREE.Mesh(geometry, material)
 
 scene.add(mesh)
+
+// DEBUG
+// gui.add(mesh.position, "y", -3, 3, 0.01)
+//Alternative way CHAINING 
+const debugObj = {
+  color: "blue",
+  spin: ()=>{
+    console.log("spin");
+  }
+}
+
+gui.addColor(debugObj, 'color' )
+  .onChange(()=>{
+    material.color.set(debugObj.color)
+  })
+
+
+gui
+  .add(mesh.position, 'y')
+  .min(-3)
+  .max(3)
+  .step(0.01)
+  .name("elevation")
+
+gui
+  .add(mesh, 'visible')
+
+gui
+  .add(material, "wireframe")
+
+  gui
+  .add(debugObj, "spin")
+
 
 const sizes = {
   width: window.innerWidth,
