25d26d7fb59dbb6d1ea05a6d3736223ea98b1a39
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-09-13 15:32:43 -0500

    materials lesson

--- src/material.js
+++ src/material.js
@@ -1,29 +1,101 @@
-import * as THREE from "three"
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-const canvas = document.querySelector(".webgl")
+THREE.ColorManagement.enabled = false
 
-//!   SCENE
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
 
-const scene = new THREE.Scene();
-const geometry = new THREE.BoxGeometry(1,1,1,2,2,2);
-const material = new THREE.MeshBasicMaterial({ color: "red" });
-const mesh = new THREE.Mesh(geometry, material)
+// Scene
+const scene = new THREE.Scene()
 
-scene.add(mesh);
+// ! OBJECTS || MESHES
+const material = new THREE.MeshBasicMaterial();
 
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 16, 16),
+  material
+)
+sphere.position.x = -1.5
+const plane = new THREE.Mesh(
+  new THREE.PlaneGeometry(1,1),
+  material
+)
+const torus = new THREE.Mesh(
+  new THREE.TorusGeometry(0.3, 0.2, 16, 32),
+  material
+)
+torus.position.x = 1.5
+
+scene.add(sphere, plane, torus)
+
+/**
+ * Sizes
+ */
 const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight
+    width: window.innerWidth,
+    height: window.innerHeight
 }
 
-const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height);
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 2
 scene.add(camera)
-camera.position.set(1,1,1)
 
-const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.outputColorSpace = THREE.LinearSRGBColorSpace
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
 
-renderer.render(scene, camera)
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
