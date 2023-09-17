000415a0f445fbb77647993ebe6b0fbd33e64982
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-09-16 23:53:47 -0500

    static updated contents

--- src/material.js
+++ src/material.js
@@ -12,6 +12,18 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+//! TEXTURES //
+
+const textureLoader = new THREE.TextureLoader()
+const doorColorTexture = textureLoader.load("/textures/door/color.jpg")
+const doorAlphaTexture = textureLoader.load("/textures/door/alpha.jpg")
+const doorHeightTexture = textureLoader.load("/textures/door/height.jpg")
+const doorRoughnessTexture = textureLoader.load("/textures/door/roughness.jpg")
+const doorMetalnessTexture = textureLoader.load("/textures/door/metalness.jpg")
+const doorNormalTexture = textureLoader.load("/textures/door/normal.jpg")
+const doorAmbientOcclusionTexture = textureLoader.load("/textures/door/ambientOcclusion.jpg")
+
+
 // ! OBJECTS || MESHES
 const material = new THREE.MeshBasicMaterial();
 
